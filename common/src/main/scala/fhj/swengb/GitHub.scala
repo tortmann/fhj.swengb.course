0a8c14085c6b517d66ac0ea89e815d2cd829f0e2
maxwageneder <maximilian.wageneder@edu.fh-joanneum.at>
2015-11-25 17:11:24 +0100

    Github.scala --> extended case Class User & GithubUserJsonFormat in order to get as much Information as we need for Avatarix

--- common/src/main/scala/fhj/swengb/GitHub.scala
+++ common/src/main/scala/fhj/swengb/GitHub.scala
@@ -5,28 +5,55 @@ import java.net.URL
 import spray.json._
 
 /**
-  * Created by lad on 13.11.15.
-  */
+ * Created by lad on 13.11.15.
+ */
 object GitHub {
 
-  case class User(login: String, avatarUrl: URL)
+  case class User(  name: String,
+                    login: String,
+                    avatarUrl: URL,
+                    userUrl: URL,
+                    htmlUrl: URL,
+                    followersUrl: URL,
+                    followingUrl: URL)
 
   object GithubUserProtocol extends DefaultJsonProtocol {
 
     implicit object GithubUserJsonFormat extends RootJsonFormat[User] {
-      def write(user: User): JsValue =
+      def write(user: User) =
         JsArray(
+          JsString(user.name),
           JsString(user.login),
-          JsString(user.avatarUrl.toString)
+          JsString(user.avatarUrl.toString),
+          JsString(user.userUrl.toString),
+          JsString(user.htmlUrl.toString),
+          JsString(user.followersUrl.toString),
+          JsString(user.followingUrl.toString)
+
+
+
         )
 
 
-      def read(value: JsValue): User = {
+      def read(value: JsValue) = {
         value match {
           case JsObject(m) =>
+            val JsString(name) = m("name")
             val JsString(login) = m("login")
             val JsString(a_url) = m("avatar_url")
-            User(login, new URL(a_url))
+            val JsString(u_url) = m("url")
+            val JsString(html_url) = m("html_url")
+            val JsString(followers_url) = m("followers_url")
+            val JsString(following_url) = m("following_url")
+
+            User( name,
+              login,
+              new URL(a_url),
+              new URL(u_url),
+              new URL(html_url),
+              new URL(followers_url),
+              new URL(following_url)
+            )
           case x =>
             deserializationError("GitHubUser expected.")
         }
